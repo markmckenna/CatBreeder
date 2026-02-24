807331fd44ed055d0e581210902b6f5bd2680eee
Mark McKenna <mark.mckenna@redspace.com>
2026-02-23 22:21:45 -0400

    Move root test config

--- config/vitest.config.ts
+++ config/vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    setupFiles: ['./src/test.ts'],
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
     coverage: {
       provider: 'v8',
